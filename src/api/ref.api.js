bb538cb09e56825ef1430a399bf69a1254548d43
Manuja Yomith <92159542+YomithK@users.noreply.github.com>
2026-04-08 09:14:27 +0530

    feat: add victim assignment UI in case detail
    - Add assignVictim API call to case.api.js
    - Add getVictimUsers API call to ref.api.js
    - Add AssignVictimModal with two modes: select from system or invite by email
    - Show victim name in case details info section
    - Wire Victim button for ADMIN and INVESTIGATOR roles

--- src/api/ref.api.js
+++ src/api/ref.api.js
@@ -1,3 +1,4 @@
 import api from './axios'
 
 export const getAssignableUsers = (params) => api.get('/ref/assignable-users', { params })
+export const getVictimUsers = (params) => api.get('/ref/victim-users', { params })
